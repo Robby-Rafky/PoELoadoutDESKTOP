4606bcafa00c5705fe60ada13d5df619684800dd
Im-a-degen <robbyrafky@gmail.com>
2022-11-01 03:00:07 +0000

    - Rough window layout added

--- userInterface/baseUI.py
+++ userInterface/baseUI.py
@@ -32,3 +32,23 @@ base_window.title("PoE Loadout Desktop")
 base_window["bg"] = D_BLUE
 
 base_window.wm_protocol("WM_DELETE_WINDOW", on_app_close)
+
+
+item_data_frame = tk.LabelFrame(base_window, text="Item Data")
+item_search_frame = tk.LabelFrame(base_window, text="Search")
+dataset_select_frame = tk.LabelFrame(base_window, text="Data Sets")
+additional_data_frame = tk.LabelFrame(base_window, text="More Data")
+general_frame = tk.LabelFrame(base_window, text="General")
+
+item_data_frame.place(height=600, width=1400, y=90, x=20)
+item_search_frame.place(height=50, width=1400, y=20, x=20)
+dataset_select_frame.place(height=350, width=1400, y=710, x=20)
+additional_data_frame.place(height=600, width=460, y=90, x=1440)
+general_frame.place(height=350, width=460, y=710, x=1440)
+
+item_data_frame["bg"], item_data_frame["fg"] = D_BLUE, WHITE
+item_search_frame["bg"], item_search_frame["fg"] = D_BLUE, WHITE
+dataset_select_frame["bg"], dataset_select_frame["fg"] = D_BLUE, WHITE
+additional_data_frame["bg"], additional_data_frame["fg"] = D_BLUE, WHITE
+general_frame["bg"], general_frame["fg"] = D_BLUE, WHITE
+
